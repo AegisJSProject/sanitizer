04dfa9d58f435a66e629edf4759cedbb63d18740
Chris Zuber <shgysk8zer0@gmail.com>
2024-04-08 17:10:25 -0700

    Update ESLint Config

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+export default {
+	'ignores': [
+		'/node_modules/*',
+		'/_site/*',
+		'/img/*',
+		'/css/*',
+		'/fonts/*',
+		'/js/std-js/*',
+		'**/*.cjs',
+		'**/*.mjs',
+		'**/*.min.js',
+		'*.out.js',
+	],
+	'rules': {
+		'indent': [2, 'tab', { 'SwitchCase': 1 }],
+		'quotes': [2, 'single'],
+		'semi': [2, 'always'],
+		'no-console': 0,
+		'no-async-promise-executor': 0,
+		'no-prototype-builtins': 0
+	},
+	'languageOptions': {
+		'globals': {
+			'globalThis': 'readonly',
+			'trustedTypes': 'readonly'
+		}
+	}
+};
